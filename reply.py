4af30375ec3d8b6ef52fe183e28bcd6315de21fb
bjong <biebergong@gmail.com>
2017-05-20 11:29:06 +0800

    translate feature

--- reply.py
+++ reply.py
@@ -1,9 +1,66 @@
+import requests
+import json
+import re
+
+
+def translate(word):
+    key = '716426270'
+    keyFrom = 'wufeifei'
+    url = 'http://fanyi.youdao.com/openapi.do?keyfrom=wufeifei&key=716426270&type=data&doctype=json&version=1.1&q=' + word
+    r = requests.get(url)
+    r_dict = json.loads(r.text)
+    try:
+        u = r_dict['basic']['us-phonetic'] # English
+        e = r_dict['basic']['uk-phonetic']
+    except KeyError:
+        try:
+            c = r_dict['basic']['phonetic'] # Chinese
+        except KeyError:
+            c = 'None'
+        u = 'None'
+        e = 'None'
+
+    try:
+        explains = r_dict['basic']['explains']
+    except KeyError:
+        explains = 'None'
+
+    reply_text = '{} {} '.format(r_dict['query'], r_dict['translation'][0])
+    if u != 'None':
+        reply_text += '(U: {} E: {} )\n'.format(u, e)
+    elif c != 'None':
+        '(Pinyin: {} )\n'.format(c)
+    else:
+        # print()
+        pass
+
+    if explains != 'None':
+        for i in range(0, len(explains)):
+            # print(explains[i])
+            reply_text += (explains[i] + '\n')
+    else:
+        # print('Explains None')
+        reply_text += 'Explains None'
+    return reply_text
+
+
 def reply(msg):
     print(msg)
 
-    if '米酱' in msg:
-        reply_text = '米酱爱你哦！'
+    # if '米酱' in msg:
+    #     reply_text = '米酱爱你哦！'
+    # else:
+    #     reply_text = '哼！'
+    if '翻译' in msg:
+        re_result = re.findall('[A-Za-z]+$', msg)
+        print(re_result)
+        word = re_result[0] if len(re_result) >= 1 else None
+        if word:
+            reply_text = translate(word)
+            return reply_text
+        else:
+            return
     else:
-        reply_text = '哼！'
+        return
 
-    return reply_text
+    # return reply_text
--- zm_chan_bot.py
+++ zm_chan_bot.py
@@ -11,8 +11,11 @@ from reply import reply
 
 def handle(msg, chat_room, s, token):
     reply_text = reply(msg)
-    url = 'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'
-    s.get(url.format(token, chat_room, reply))
+    if reply_text:
+        url = 'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'
+        s.get(url.format(token, chat_room, reply_text))
+    else:
+        pass
 
 
 def get_updates(s, offset, token):
@@ -21,6 +24,23 @@ def get_updates(s, offset, token):
     json_ = r.content.decode()
     history = json.loads(json_)
     return history
+
+
+def allow_reply(result):
+    msg = result['message'].get('text')
+    # i['message']['from'].get('username') == 'bjong' and\
+    if msg:
+        if ('@zm_chan_bot' in msg or msg.startswith('米酱')) or\
+           (
+               result['message']['chat']['type'] == 'private' and
+               result['message']['from'].get('username') == 'bjong'
+           ):
+            return True
+        else:
+            return False
+
+    else:
+        return False
 
 
 def loop(s, offset, token):
@@ -32,21 +52,23 @@ def loop(s, offset, token):
 
         result = history['result']
         for i in result:
-            if i['update_id'] > offset:
-                print(i)
+            if i['update_id'] > offset and i.get('message'):
+                pprint.pprint(i)
                 msg = i['message'].get('text')
-                if not msg:
-                    continue
-                chat_room = i['message']['chat']['id']
-                handle(msg, chat_room, s, token)
+                if allow_reply(i):
+                    chat_room = i['message']['chat']['id']
+                    handle(msg, chat_room, s, token)
+                else:
+                    pass
             else:
                 pass
+
         offset = history['result'][-1]['update_id'] if len(history['result']) >= 1 else 0
         time.sleep(1)
 
 
 def zm_chan_bot_start(cfg):
-    offset, token = cfg['offset'], cfg['token']
+    token = cfg['token']
     s = requests.session()
     s.proxies = {'http': 'socks5://127.0.0.1:1080',
                  'https': 'socks5://127.0.0.1:1080'}
@@ -54,8 +76,8 @@ def zm_chan_bot_start(cfg):
     # r = s.get(url.format(token, offset))
     # json_ = r.content.decode()
     # history = json.loads(json_)
-    history = get_updates(s, offset, token)
-    print(history)
+    history = get_updates(s, 0, token)
+    pprint.pprint(history)
     offset = history['result'][-1]['update_id'] if len(history['result']) >= 1 else 0
     # if len(history['result']) >= 1:
     #     offset = history['result'][-1]['update_id']
