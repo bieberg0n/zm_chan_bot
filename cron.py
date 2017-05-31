da2ee47e31b4b9ef2407a33ac1c5b1999c701d07
bjong <biebergong@gmail.com>
2017-05-31 10:50:19 +0800

    auto notice

--- cron.py
+++ cron.py
@@ -1,12 +1,14 @@
-import time
+from datetime import datetime, timedelta
 
 def cron():
-    get_second = lambda: int(time.time())
-    next_time = get_second() + 180
+    # get_second = lambda: int(time.time())
+    now = datetime.now()
+    next_time = now.replace(hour=9, minute=0) + timedelta(days=1)
+    print(next_time)
     while True:
-        now = get_second()
+        now = datetime.now()
         if now >= next_time:
-            yield '骚扰一下' + ' ' + time.ctime()
-            next_time = now + 180
+            yield 'BJ记得打卡！'
+            next_time = next_time + timedelta(days=1)
         else:
             yield
